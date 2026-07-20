ca1752abf9a69c1a8b1bc9d2b82af7aa167c75d5
dhananjay852714-star <dhananjay852714@gmail.com>
2026-07-20 12:05:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # Render mein set ki gayi DATABASE_URL ka use karo
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
-# Naya route jisse tumhara index.html dikhega
 @app.route('/')
 def home():
     with open('index.html', 'r') as f:
@@ -15,20 +14,29 @@ def home():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # HTML form se data uthao
-    name = request.form.get('name')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    mobile = request.form.get('mobile')
+    try:
+        # HTML form se data uthao
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        mobile = request.form.get('mobile')
 
-    # Neon se connect karo
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
+        # Neon se connect karo
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
 
-    # Table mein insert karo
-    cur.execute("INSERT INTO users (name, email, password, mobile) VALUES (%s, %s, %s, %s)",
-                (name, email, password, mobile))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return "Data Save ho gaya!"
+        # Table mein insert karo
+        cur.execute("INSERT INTO users (name, email, password, mobile) VALUES (%s, %s, %s, %s)",
+                    (name, email, password, mobile))
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+        
+        return "<h1>Data Save ho gaya!</h1><br><a href='/'>Wapas form par jayein</a>"
+    
+    except Exception as e:
+        return f"<h1>Error aaya:</h1> {str(e)}<br><a href='/'>Try again</a>"
+
+if __name__ == '__main__':
+    app.run()
