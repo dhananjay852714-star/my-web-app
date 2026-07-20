24ea83bef5c653d5949702f9aaf6955935f554b3
dhananjay852714-star <dhananjay852714@gmail.com>
2026-07-20 14:05:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
 import os
 import psycopg2
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Render mein set ki gayi DATABASE_URL ka use karo
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 @app.route('/')
 def home():
-    with open('index.html', 'r') as f:
-        return render_template_string(f.read())
+    # Ye ab templates folder se index.html uthayega
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     try:
-        # HTML form se data uthao
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
         mobile = request.form.get('mobile')
 
-        # Neon se connect karo
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
-
-        # Table mein insert karo
         cur.execute("INSERT INTO users (name, email, password, mobile) VALUES (%s, %s, %s, %s)",
                     (name, email, password, mobile))
-        
         conn.commit()
         cur.close()
         conn.close()
